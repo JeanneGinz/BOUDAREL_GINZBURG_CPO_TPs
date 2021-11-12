249a5754802ba3ad2d3c11f9241023ea8576c361
luluxontheflux <luluxontheflux@localhost>
2021-11-12 14:09:22 +0100

    Manip exo 3

---
+++ TP2_manip_BOUDAREL_GINZBURG/src/tp2_manip_boudarel_ginzburg/TP2_manip_BOUDAREL_GINZBURG.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_manip_boudarel_ginzburg;
+
+/**
+ *
+ * @author luluxontheflux
+ */
+public class TP2_manip_BOUDAREL_GINZBURG {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+    }
+    
+}
