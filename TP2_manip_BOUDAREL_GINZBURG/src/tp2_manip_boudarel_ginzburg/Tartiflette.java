3baca02eaf16e124f402180d196c9842689214a9
luluxontheflux <luluxontheflux@localhost>
2021-11-12 14:37:20 +0100

    Manip exo 3

--- TP2_manip_BOUDAREL_GINZBURG/src/tp2_manip_boudarel_ginzburg/Tartiflette.java
+++ TP2_manip_BOUDAREL_GINZBURG/src/tp2_manip_boudarel_ginzburg/Tartiflette.java
@@ -2,11 +2,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package tp2_manip_boudarel_ginzburg;
 
 /**
  *
  * @author luluxontheflux
  */
 public class Tartiflette {
+    int nbCalories;
     
+     public Tartiflette (int val){
+      nbCalories = val;
+  }
 }
