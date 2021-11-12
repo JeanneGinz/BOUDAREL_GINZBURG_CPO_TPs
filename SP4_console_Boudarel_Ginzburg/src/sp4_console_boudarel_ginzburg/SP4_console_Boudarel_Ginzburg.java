3e4e3cde8708fed2f8ccf9087d524473f39a5285
jeann <jeann@192.168.115.84>
2021-11-12 16:10:36 +0100

    creation projet Puissance4

---
+++ SP4_console_Boudarel_Ginzburg/src/sp4_console_boudarel_ginzburg/SP4_console_Boudarel_Ginzburg.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sp4_console_boudarel_ginzburg;
+
+/**
+ *
+ * @author jeann
+ */
+public class SP4_console_Boudarel_Ginzburg {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
