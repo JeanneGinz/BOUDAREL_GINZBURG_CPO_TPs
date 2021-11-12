9104ab54ab00b488ebfd0011cce3f0dfe90269db
luluxontheflux <luluxontheflux@localhost>
2021-11-12 18:06:09 +0100

    Joueur définit, Couleur joueurs définit

--- SP4_console_Boudarel_Ginzburg/src/sp4_console_boudarel_ginzburg/Joueur.java
+++ SP4_console_Boudarel_Ginzburg/src/sp4_console_boudarel_ginzburg/Joueur.java
@@ -17,26 +17,26 @@ int nombreDesintegrateurs, nombreJetonsRestants;//initialisation des valeurs
 
 Jeton[] ListeJetons= new Jeton[21]; // Permet au joueur d'avoir des jetons 
 
-public String affecterCouleur (String couleur) { //Création de la méthode affectant les couleurs de pions aux joueurs 
-//public void affecterCouleur (String couleur) { 
 
-    Random random = new Random(); // initialisation de notre fonctions random
-    int nb; //initialisation d'un nombre pour y affecter la randomisation
-    nb = random.nextInt(2); //Génére un chiffre entre 0 et 1
+public Joueur(String n){   
+    Nom=n;
+    
+}
 
-if (nb==0){ //si le chiffre est 0, le joueur sera rouge
-    couleur="rouge";  }
-else   couleur="jaune";  //si le chiffre est 1, le joueur sera jaune
 
-return couleur;
+public void affecterCouleur (String c) { //Création de la méthode affectant les couleurs de pions aux joueurs 
+Couleur=c;   
+}
+
+
    
 }
 
-public boolean ajouterJeton(Jeton listeJeton) {
+//public boolean ajouterJeton(Jeton listeJeton) {
     
     
-    
-}
+  //return false;  
+//}
 
 
 
--- SP4_console_Boudarel_Ginzburg/src/sp4_console_boudarel_ginzburg/SP4_console_Boudarel_Ginzburg.java
+++ SP4_console_Boudarel_Ginzburg/src/sp4_console_boudarel_ginzburg/SP4_console_Boudarel_Ginzburg.java
@@ -4,6 +4,9 @@
  */
 package sp4_console_boudarel_ginzburg;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author jeann
@@ -17,7 +20,48 @@ public class SP4_console_Boudarel_Ginzburg {
      
   System.out.println("Bienvenue sur le Super Puissance 4"); 
   
-     
+ Scanner sc; //initialisation du scanner
+ sc=new Scanner (System.in);
+ String nom1, nom2;
+ 
+ //Créations des joueurs et de leurs noms
+   System.out.println("Comment s'appelle le premier joueur ?"); //demande des pseusos
+   nom1 = sc.nextLine(); //Enregistrement dans la mémoire des pseudos
+  
+   Joueur joueur1; //Création de l'objet étant le premier joueur
+   joueur1=new Joueur(nom1); //affectation de son nom dans la classe
+  
+   System.out.println("Comment s'appelle le deuxième joueur ?");//Création de l'objet étant le deuxième joueur
+   nom2 = sc.nextLine();
+      
+   Joueur joueur2;//Création de l'objet étant le premier joueur
+   joueur2=new Joueur(nom2);//affectation de son nom dans la classe
+  
+ //Affecatation des couleurs et ordre de jeu
+  String couleur;
+  
+  Random random = new Random(); // initialisation de notre fonctions random
+    int nb; //initialisation d'un nombre pour y affecter la randomisation
+    nb = random.nextInt(2); //Génére un chiffre entre 0 et 1
+if (nb==0){ //si le chiffre est 0, le joueur sera rouge
+      couleur = "rouge";
+      joueur1.affecterCouleur(couleur); //joueur 1 devient rouge
+      joueur2.affecterCouleur("jaune"); //joueur 2 devient jaune
+ }
+else  { couleur="jaune";  //si le chiffre est 1, le joueur sera jaune*/
+      joueur1.affecterCouleur(couleur);  //joueur 1 devient jaune
+      joueur2.affecterCouleur("rouge"); }//joueur 2 devient rouge
+
+      nb=0;
+      nb = random.nextInt(2);
+      if (nb==0){
+          
+      }
+      
+      
+      
+      
+      
     }
     
 }
