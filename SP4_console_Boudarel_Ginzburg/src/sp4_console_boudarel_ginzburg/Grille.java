84a7343a9193d13fc4a17454009e3e038b2e0a5f
luluxontheflux <luluxontheflux@localhost>
2021-11-15 11:19:25 +0100

    Classe Cellule,joueur,jetons en 1.0, creation grille

--- SP4_console_Boudarel_Ginzburg/src/sp4_console_boudarel_ginzburg/Grille.java
+++ SP4_console_Boudarel_Ginzburg/src/sp4_console_boudarel_ginzburg/Grille.java
@@ -10,6 +10,21 @@ package sp4_console_boudarel_ginzburg;
  */
 public class Grille {
     
+Cellule[][] CellulesJeu = new Cellule[6][7]; // Création de l'objet de type de cellule
+    
+    public Grille(){
+        for(int i=0; i<6; i++){
+            for(int j=0; j<7;j++){
+                CellulesJeu[i][j]= null;
+            }
+        }
+            
+    } 
+    public boolean ajouterJetonDansColonne(Jeton, int ajou) {
+        
+
+    }
+    
     
     
     
