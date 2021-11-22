be1270f23e87e6c7f7bf9327c6ca4a68c3fed24d
jeann <jeann@192.168.115.84>
2021-11-22 17:08:49 +0100

    debuterPartie()

--- SP4_console_Boudarel_Ginzburg/src/sp4_console_boudarel_ginzburg/Partie.java
+++ SP4_console_Boudarel_Ginzburg/src/sp4_console_boudarel_ginzburg/Partie.java
@@ -5,7 +5,7 @@
 package sp4_console_boudarel_ginzburg;
 
 import java.util.Random;
-
+import java.util.Scanner;
 /**
  *
  * @author jeann
@@ -30,10 +30,25 @@ public class Partie {
         }
         
       int nb=0; // initialisation pour determiner le joueur qui commence
+       Random random = new Random(); // initialisation de notre fonctions random
       nb = random.nextInt(2);
-      if (nb==0){ // si nb=0 alors le joueur jaune commence 
-          String coul=2
+      if (nb==0){ // si nb=0 alors le joueur rouge commence 
+         String coul="rouge";
+             if (coul==ListeJoueurs[0].Couleur){
+             joueurCourant = ListeJoueurs[0];
+         }
+         else{ joueurCourant=ListeJoueurs[1];}
+         
+      }
+       if (nb==1){ // si nb=0 alors le joueur jaune commence 
+         String coul="jaune";
+         if (coul==ListeJoueurs[0].Couleur){
+             joueurCourant = ListeJoueurs[0];
+         }
+         else{ joueurCourant=ListeJoueurs[1];}
+         
         }
+      
       
     }
     
@@ -47,16 +62,47 @@ public class Partie {
 if (nb==0){ //si le chiffre est 0, le joueur sera rouge
       couleur = "rouge";
       ListeJoueurs[0].affecterCouleur(couleur); //joueur 1 devient rouge
-      joueur2.affecterCouleur("jaune"); //joueur 2 devient jaune
+      ListeJoueurs[1].affecterCouleur("jaune"); //joueur 2 devient jaune
  }
 else  { couleur="jaune";  //si le chiffre est 1, le joueur sera jaune*/
-      joueur1.affecterCouleur(couleur);  //joueur 1 devient jaune
-     joueur2.affecterCouleur("rouge"); }//joueur 2 devient rouge
+      ListeJoueurs[0].affecterCouleur(couleur);  //joueur 1 devient jaune
+     ListeJoueurs[1].affecterCouleur("rouge"); }//joueur 2 devient rouge
     }
 
 
     
 public void debuterPartie(){
+
+    
+    grilleJeu.viderGrille(); // on vérifie ue la grille soit bien vide pour ne pas créer de pbs
+    initialiserPartie(); // préparation de la grille de jeu
+    
+    System.out.println("Bienvenue sur le Super Puissance 4"); 
+  
+ Scanner sc; //initialisation du scanner
+ sc=new Scanner (System.in);
+ String nom1, nom2;
+ 
+ //Créations des joueurs et de leurs noms
+   System.out.println("Comment s'appelle le premier joueur ?"); //demande des pseusos
+   nom1 = sc.nextLine(); //Enregistrement dans la mémoire des pseudos
+  
+   Joueur joueur1; //Création de l'objet étant le premier joueur
+   joueur1=new Joueur(nom1); //affectation de son nom dans la classe
+  
+   System.out.println("Comment s'appelle le deuxième joueur ?");//Création de l'objet étant le deuxième joueur
+   nom2 = sc.nextLine();
+      
+   Joueur joueur2;//Création de l'objet étant le premier joueur
+   joueur2=new Joueur(nom2);//affectation de son nom dans la classe
+   
+    initialiserPartie(); // préparation de la grille de jeu
+    
+   //boucle verifie while 
+   while(){
+       
+       
+   }
 
 }
 }
